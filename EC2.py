f1c3624a45103c5102ce093eb5d6bb357dba24bd
Alon1s <71959905+Alon1s@users.noreply.github.com>
2020-10-14 16:03:57 +0300

    EC2

--- EC2.py
+++ EC2.py
@@ -1,5 +1,6 @@
 import boto3
 
+ec2 = boto3.resource('ec2')
 while("True"):
     choice = input("Here are some options: \n1.Describe your instances \n2.Deploy your instances \n3.Destroy your instances \n4.Stop your instances \n5.Start your instances\n")
 #Describe an instance
@@ -11,7 +12,6 @@ while("True"):
                 print("ID: " + y['InstanceId'] + "\nIP Address: " + y['PublicIpAddress'])
 # create a new EC2 instance
     elif (choice == "2"):
-        ec2 = boto3.resource('ec2')
         instances = ec2.create_instances(
             ImageId='ami-0bbe28eb2173f6167',
             MinCount=1,
@@ -23,7 +23,6 @@ while("True"):
     elif (choice == "3"):
         instances = input("enter the ids of the instances that you want to stop:")
         ids = [instances]
-        ec2 = boto3.resource('ec2')
         ec2.instances.filter(InstanceIds=ids).terminate()
 
 
@@ -31,14 +30,12 @@ while("True"):
     elif (choice == "4"):
         instances = input("enter the ids of the instance that you want to stop:")
         ids = [instances]
-        ec2 = boto3.resource('ec2')
         ec2.instances.filter(InstanceIds=ids).stop()
 
 #Start an instnace
     elif (choice == "5"):
         instances = input("enter the ids of the instance that you want to start:")
         ids = [instances]
-        ec2 = boto3.client('ec2')
         ec2.start_instances(InstanceIds=ids)
         
     exit= input("Do you want something else? y/n")
