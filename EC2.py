a30131bb6d8ad2c457615b74fa790e0ce7583657
Alon1s <71959905+Alon1s@users.noreply.github.com>
2020-10-17 15:45:14 +0300

    ec2.py

--- EC2.py
+++ EC2.py
@@ -1,9 +1,9 @@
 import boto3
 
 ec2 = boto3.resource('ec2')
-while("True"):
-    option = input("Here are some options:\n1.create an instances \n2.Start your instance \n3.Stop your instance \n4.Terminate your instance\n")
-# create a new EC2 instance
+while ("True"):
+    option = input("Here are some options:\n1.create an instances \n2.Start your instance \n3.Stop your instance \n4.Terminate your instance\n5.EC2 Describe")
+    # create a new EC2 instance
     if (option == "1"):
         instances = ec2.create_instances(
             ImageId=input("Pleae fill an ImageId: "),
@@ -12,34 +12,48 @@ while("True"):
             InstanceType=input("Pleae fill the Instace type you want: "),
             KeyName=input("Pleae fill a KeyName: ")
         )
-        break
-#Start 
+    # Start
     elif (option == "2"):
         tagname = input("Please fill an instance name: ")
         tagvalue = input("Please fill a Value: ")
         ec2.instances.filter(Filters=[
-            {'Name': 'tag': [tagname], 'Values': [tagvalue]},
-            {'Name': 'instance-state-name', 'Values': ['running']}
+        {'Name': 'tag': [tagname],'Values': [tagvalue]},
+        {'Name': 'instance-state-name', 'Values': ['running']}
         ]).start()
-        break
-#Stop
+
+
+    # Stop
     elif (option == "3"):
         tagname = input("Please fill an instance name: ")
         tagvalue = input("Please fill a Value: ")
         ec2.instances.filter(Filters=[
-            {'Name': 'tag': [tagname], 'Values': [tagvalue]},
-            {'Name': 'instance-state-name', 'Values': ['stopped']}
+        {'Name': 'tag': [tagname], 'Values': [tagvalue]},
+        {'Name': 'instance-state-name', 'Values': ['stopped']}
         ]).stop()
-        break
-#Terminate 
+
+    # Terminate
     elif (option == "4"):
         tagname = input("Please fill an instance name: ")
         tagvalue = input("Please fill a Value: ")
         ec2.instances.filter(Filters=[
-            {'Name': 'tag': [tagname], 'Values': [tagvalue]},
-            {'Name': 'instance-state-name', 'Values': ['terminating']}
+        {'Name': 'tag': [tagname], 'Values': [tagvalue]},
+        {'Name': 'instance-state-name', 'Values': ['terminating']}
         ]).terminate()
-        break
+
+    elif (choice == "5"):
+        client = boto3.client('ec2')
+        response = client.describe_instances()
+        for a in response['Reservations']:
+            for b in a['Instances']:
+                print("ID: " + b['InstanceId'] + "\nIP Address: " + b['PublicIpAddress'])
+
     else:
-        print("Only numbers between 1-4")
+        print("Only numbers between 1-4 ")
+    
+    exit = input("Do you need something else? y/n ")
+    if (exit == "yes" or exit == "y"):
         continue
+    else:
+        break
+
+print("Were done!")
